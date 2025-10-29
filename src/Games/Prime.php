136142650595f5ee5222c8cb4a6c10c7b2d04267
Elizaveta Galimova <egalimova@marfa-tech.com>
2025-10-29 22:19:26 +0500

    GG

---
+++ src/Games/Prime.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hexlet\Code\Games\Prime;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function generateRoundData(): array
+{
+    $num = rand(1, 100);
+    $question = (string)$num;
+    $result = '';
+    $limit = sqrt($num);
+
+    if ($num < 2) {
+        $result = 'no';
+    } elseif ($num === 2) {
+        $result = 'yes';
+    } elseif ($num % 2 === 0) { // Проверка на четность, кроме 2
+        $result = 'no';
+    } else {
+        $result = 'yes';
+        for ($i = 3; $i <= $limit; $i += 2) {
+            if ($num % $i === 0) {
+                $result = 'no';
+                break;
+            }
+        }
+    }
+
+    return [
+        'question' => $question,
+        'correct_answer' => (string)$result,
+    ];
+}
